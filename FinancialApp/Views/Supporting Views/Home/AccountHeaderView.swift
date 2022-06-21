2e51a3eb79af31db8cbb8ba2d65f4188246488b7
Esteban Calvete <esteban.calvete@gmail.com>
2022-06-21 21:46:12 +0200

    Add supporting views for AccoutnHeader, CardView and HomeSubmenu

--- FinancialApp/Views/Supporting Views/Home/AccountHeaderView.swift
+++ FinancialApp/Views/Supporting Views/Home/AccountHeaderView.swift
@@ -9,12 +9,24 @@ import SwiftUI
 
 struct AccountHeaderView: View {
     var body: some View {
-        Text("Account Header View")
+        HStack(alignment: .center) {
+            VStack(alignment: .leading, spacing: -8) {
+                Text("Hi, Esteban Calvete")
+                    .customFont(.custom(.bold, 24))
+                Text("Welcome to the FinancialApp.")
+                    .customFont(.custom(.demibold, 16))
+            }
+            .foregroundColor(.basePrussianBlue)
+            Spacer()
+            Image("avatar")
+        }
+        .padding(.horizontal, 20)
     }
 }
 
 struct AccountHeaderView_Previews: PreviewProvider {
     static var previews: some View {
         AccountHeaderView()
+            .previewLayout(.fixed(width: 600, height: 80))
     }
 }
--- FinancialApp/Views/Supporting Views/Home/HomeSubmenuView.swift
+++ FinancialApp/Views/Supporting Views/Home/HomeSubmenuView.swift
@@ -9,12 +9,71 @@ import SwiftUI
 
 struct HomeSubmenuView: View {
     var body: some View {
-        Text("Home Submenu View")
+        VStack(spacing: 0) {
+            HStack {
+                Text("QUICK SERVICES")
+                    .customFont(.custom(.bold, 20))
+                    .padding(.bottom, 2)
+                    .foregroundColor(Color.basePrussianBlue)
+                Spacer()
+            }
+            Divider()
+                .padding(.bottom, 10)
+            HStack(alignment: .center, spacing: 16) {
+                Button(action: {}) {
+                    VStack {
+                        Image("icon-send")
+                        Text("ADD NEW")
+                            .customFont(.custom(.demibold, 12))
+                    }
+                    .frame(width: 75, height: 80)
+                    .background(Color.white)
+                    .cornerRadius(10)
+                }
+                Spacer()
+                Button(action: {}) {
+                    VStack {
+                        Image("icon-receive")
+                        Text("CARDS")
+                            .customFont(.custom(.demibold, 12))
+                    }
+                    .frame(width: 75, height: 80)
+                    .background(Color.white)
+                    .cornerRadius(10)
+                }
+                Spacer()
+                Button(action: {}) {
+                    VStack {
+                        Image("icon-invoices")
+                        Text("LOCATIONS")
+                            .customFont(.custom(.demibold, 12))
+                    }
+                    .frame(width: 75, height: 80)
+                    .background(Color.white)
+                    .cornerRadius(10)
+                }
+                Spacer()
+                Button(action: {}) {
+                    VStack {
+                        Image("icon-bills")
+                        Text("TRANSACTIONS")
+                            .customFont(.custom(.demibold, 12))
+                    }
+                    .frame(width: 75, height: 80)
+                    .background(Color.white)
+                    .cornerRadius(10)
+                }
+            }
+            .foregroundColor(.basePrussianBlue)
+            .padding(10)
+        }
+        .background(Color.baseWhite)
     }
 }
 
 struct HomeSubmenuView_Previews: PreviewProvider {
     static var previews: some View {
         HomeSubmenuView()
+            .previewLayout(.fixed(width: 600, height: 140))
     }
 }
