b15ac9368d1d927ade312b0184bd6ff479e6a25b
Esteban Calvete <esteban.calvete@gmail.com>
2022-06-28 20:49:51 +0200

    Add account form view

--- FinancialApp/Views/Supporting Views/Create Account/AccountFormView.swift
+++ FinancialApp/Views/Supporting Views/Create Account/AccountFormView.swift
@@ -11,17 +11,31 @@ struct AccountFormView: View {
     private let type = "creditcard"
     
     init() {
-        
+        UITableView.appearance().backgroundColor = UIColor.baseWhite
+        UITableView.appearance().tableFooterView = UIView(frame: CGRect(x: 0, y: 0, width: 0, height: Double.leastNonzeroMagnitude))
+        UITableView.appearance().tableHeaderView = UIView(frame: CGRect(x: 0, y: 0, width: 0, height: Double.leastNonzeroMagnitude))
+        UITableView.appearance().separatorStyle = .none
     }
     
     var body: some View {
-        Text("Account Form View")
+        List {
+            Section {
+                FirstNameView()
+                LastNameView()
+                CardLimitView()
+            }
+            .listRowBackground(Color.baseWhite)
+            .listRowInsets(EdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 0))
+        }
+        .listStyle(GroupedListStyle())
+        .background(Color.baseWhite)
     }
 }
 
 struct AccountFormView_Previews: PreviewProvider {
     static var previews: some View {
         AccountFormView()
+            .previewLayout(.fixed(width: 600, height: 200))
     }
 }
 
