ee5524446e6d0ff8549724593cffa4c06a4b3b16
Esteban Calvete <esteban.calvete@gmail.com>
2022-07-01 17:49:07 +0200

    Account list view and card list row view add

--- FinancialApp/Views/AccountListView.swift
+++ FinancialApp/Views/AccountListView.swift
@@ -10,10 +10,38 @@ import SwiftUI
 struct AccountListView: View {
     
     init() {
+        UITableViewCell.appearance().backgroundColor = .clear
+        UITableView.appearance().backgroundColor = .clear
     }
     
     var body: some View {
-        Text("Accoutn List View")
+        NavigationView{
+            ZStack(alignment: .leading) {
+                Color.baseWhite
+                List {
+                    ForEach(0..<3) { account in
+                        HStack {
+                            Spacer()
+                            NavigationLink(destination: AccountHomeView()) {
+                                CardListRow()
+                            }
+                            .buttonStyle(PlainButtonStyle())
+                            Spacer()
+                        }
+                        .padding()
+                    }
+                    .listRowSeparator(.hidden)
+                    .listRowInsets(EdgeInsets())
+                }
+            }
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationTitle("Accounts")
+            .toolbar {
+                Button(action: {}) {
+                    Text("Add New")
+                }
+            }
+        }
     }
 }
 
--- FinancialApp/Views/Supporting Views/Misc/CardListRow.swift
+++ FinancialApp/Views/Supporting Views/Misc/CardListRow.swift
@@ -9,7 +9,74 @@ import SwiftUI
 
 struct CardListRow: View {
     var body: some View {
-        Text("Card List Row")
+        ZStack {
+            HStack(spacing: -116) {
+                Image("left-card")
+                    .renderingMode(.template)
+                    .foregroundColor(.black)
+                Image("right-card")
+                    .renderingMode(.template)
+                    .foregroundColor(.black)
+                    .opacity(0.94)
+            }
+            VStack {
+                HStack {
+                    Image("checkmark")
+                    VStack(alignment: .leading, spacing: -6) {
+                        Text("$99,999")
+                            .customFont(.custom(.bold, 20))
+                        Text("Available Balance")
+                            .customFont(.custom(.ultralight, 10))
+                    }
+                    Spacer()
+                    Image("visa-logo")
+                }
+                .padding(.horizontal, 20)
+                HStack {
+                    HStack {
+                        Text("****")
+                            .customFont(.custom(.black, 17))
+                        Text("****")
+                            .customFont(.custom(.black, 17))
+                        Text("****")
+                            .customFont(.custom(.black, 17))
+                        Text("9999")
+                            .customFont(.custom(.black, 20))
+                    }
+                    Spacer()
+                }
+                .padding(.top, 15)
+                .padding(.horizontal, 20)
+                Spacer()
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("CARD HOLDER")
+                            .customFont(.custom(.bold, 11))
+                            .foregroundColor(Color.baseRockBlue)
+                        Text("Esteban Calvete")
+                            .customFont(.custom(.black, 16))
+                    }
+                    Spacer()
+                    VStack(alignment: .leading) {
+                        Text("EXPIRES")
+                            .customFont(.custom(.bold, 11))
+                            .foregroundColor(Color.baseRockBlue)
+                        Text("08/22")
+                            .customFont(.custom(.black, 16))
+                    }
+                    VStack(alignment: .leading) {
+                        Text("CVV")
+                            .customFont(.custom(.bold, 11))
+                            .foregroundColor(Color.baseRockBlue)
+                        Text("999")
+                            .customFont(.custom(.black, 16))
+                    }
+                }
+            }
+            .frame(width: 280, height: 160)
+            .padding(10)
+            .foregroundColor(.baseWhite)
+        }
     }
 }
 
